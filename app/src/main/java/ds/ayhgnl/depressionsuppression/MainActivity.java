fb47678c0a2af3a72a79804d80b06b6ce9fbbcb9
Shuryu12 <winston.yin109@gmail.com>
2019-10-11 13:51:11 -0400

    question template and some sharedpref stuff

--- app/src/main/java/ds/ayhgnl/depressionsuppression/MainActivity.java
+++ app/src/main/java/ds/ayhgnl/depressionsuppression/MainActivity.java
@@ -2,6 +2,7 @@ package ds.ayhgnl.depressionsuppression;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = this.getSharedPreferences("com.answer.storage", Context.MODE_PRIVATE);
+        String intialQuestionsKey = "com.answer.storage.init";
     }
 }
