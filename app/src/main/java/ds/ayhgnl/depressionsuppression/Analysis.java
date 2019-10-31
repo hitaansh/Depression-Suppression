0dc80fe795075b1800de36694d26b360720c0a17
hitaansh <hitaanshgaur@gmail.com>
2019-10-30 23:36:45 -0400

    app done

--- app/src/main/java/ds/ayhgnl/depressionsuppression/Analysis.java
+++ app/src/main/java/ds/ayhgnl/depressionsuppression/Analysis.java
@@ -23,14 +23,17 @@ import java.util.Calendar;
 public class Analysis extends AppCompatActivity {
     SharedPreferences pref;
     int maxScore = 60, score, tier1 = 16, tier2 = 24, tier3 = 36, tier4 = 42, tier5 = 48, tier6 = 54, tier7 = 60;
-    int interest; int appetite; int sleep; int concentration; int worthlessness; int movement; int fatigue; int suicide;
-    TextView scoreBox, appetiteBox, interestBox, sleepBox, concentrationBox, worthlessnessBox, fatigueBox, movementBox, suicideBox, tier, condition, time;
+    int interest; int appetite; int sleep; int concentration; int worthlessness; int movement; int fatigue; int suicide; int oldScore;
+    TextView scoreBox, appetiteBox, interestBox, sleepBox, concentrationBox, worthlessnessBox, fatigueBox, movementBox, suicideBox, tier, condition, time, act1, act2, act3;
     CountDownTimer timer;
     String base = "You are at";
     String good = "You are in a good place of mind. Continue to stay positive and do enjoyable things!";
     String bad = "You are not in a good place of mind. Try to stay positive and do activites you enjoy. Your emergency contacts have been informed";
     public static final String CHANNEL_1_ID = "channel";
     private NotificationManagerCompat notificationManager;
+    private boolean timerRunning;
+    private long timeLeftMillis, endTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +50,10 @@ public class Analysis extends AppCompatActivity {
         tier = findViewById(R.id.tier);
         condition = findViewById(R.id.condition);
         time = findViewById(R.id.timeLeft);
+        act1 = findViewById(R.id.activity1);
+        act2 = findViewById(R.id.activity2);
+        act3 = findViewById(R.id.activity3);
+
 
         pref = this.getSharedPreferences("com.answer.storage", Context.MODE_PRIVATE);
         scoreBox.setText(pref.getInt("Score", 0) + "");
@@ -67,6 +74,10 @@ public class Analysis extends AppCompatActivity {
         movement = pref.getInt("Movement", 0);
         suicideBox.setText(pref.getInt("SuicidalIntention", 0) + "");
         suicide = pref.getInt("SuicidalIntention", 0);
+        act1.setText(pref.getString("Act1", ""));
+        act2.setText(pref.getString("Act2", ""));
+        act3.setText(pref.getString("Act3", ""));
+        oldScore = pref.getInt("OldScore", -1);
         int [] subscores = {interest, appetite, sleep, concentration, worthlessness, fatigue, movement, suicide};
         String[] categories = {"interest", "appetite", "sleep", "concentration", "worthlessness", "fatigue", "movement", "suicide"};
         //if sadness  or sleep are 8 or greater, it is concerning
@@ -119,12 +130,32 @@ public class Analysis extends AppCompatActivity {
                 condition.setText(bad);
                 condition.setTextColor(-65536); // red color
             }
-            text(concerns);
-
+            // text(concerns);
         }
+
+        if(oldScore != -1) {
+            double change = score - oldScore;
+            double s = oldScore;
+            if((change/s) > 0.3){
+                String drop = "Your score has increased by more than 30% since your last survey. You are entering a dangerous state of mind. Try to stay positive and do activites you enjoy. Your emergency contacts have been informed";
+                condition.setText(drop);
+                condition.setTextColor(-65536); // red color
+                //text(concerns);
+            }
+        }
+
+
+        startTimer();
+
+    }
+
+    private void startTimer() {
+        //endTime = System.currentTimeMillis() + timeLeftMillis;
         timer = new CountDownTimer(10000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
+                timerRunning = true;
+                timeLeftMillis = millisUntilFinished;
                 long hours = (millisUntilFinished)/(3600000);
                 long minutes = (millisUntilFinished)/(60000) - (hours * 60);
                 long seconds = (millisUntilFinished)/(1000) - (minutes * 60) - (hours * 3600);
@@ -136,24 +167,19 @@ public class Analysis extends AppCompatActivity {
             @Override
             public void onFinish() {
                 notifySurvey();
+                timerRunning = false;
                 pref = getSharedPreferences("com.answer.storage", Context.MODE_PRIVATE);
                 SharedPreferences.Editor edit = pref.edit();
                 edit.putBoolean("quizDone", false);
+                edit.putInt("OldScore", score);
+                edit.putInt("Score", 0);
                 edit.apply();
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         };
         timer.start();
-
-    }
-
-    private void startAlarm(Calendar c) {
-        // System.out.println("here at alarm");
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlertReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
-        alarmManager.setExact(AlarmManager.RTC, c.getTimeInMillis(), pendingIntent);
+        timerRunning = true;
     }
 
     private void notifySurvey() {
@@ -210,4 +236,35 @@ public class Analysis extends AppCompatActivity {
             smgr.sendTextMessage(emg3, null, Message, null, null);
         }
     }
+
+//    @Override
+//    public void onStop(){
+//        super.onStop();
+//        pref = this.getSharedPreferences("com.answer.storage", Context.MODE_PRIVATE);
+//        SharedPreferences.Editor edit = pref.edit();
+//        edit.putLong("mLeft", timeLeftMillis);
+//        edit.putLong("endTime", endTime);
+//        edit.putBoolean("running", timerRunning);
+//        edit.apply();
+//        if(timer != null) {
+//            timer.cancel();
+//        }
+//    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        pref = this.getSharedPreferences("com.answer.storage", Context.MODE_PRIVATE);
+//        timeLeftMillis = pref.getLong("mLeft", 0);
+//        timerRunning = pref.getBoolean("running", false);
+//
+//        if(timerRunning) {
+//            endTime = pref.getLong("endTime", 0);
+//            timeLeftMillis = endTime - System.currentTimeMillis();
+//
+//            if(timeLeftMillis < 0) {
+//                timeLeftMillis = 0;
+//            }
+//        }
+//    }
 }
